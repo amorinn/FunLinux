e78717adff49e057653d149e2096c7097db8eb12
Martín Amorín <83926750+amorinn@users.noreply.github.com>
2021-11-06 00:23:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 apt install feh lolcat pv sox libsox-fmt-all
 cp ./funlinux /bin
 chmod +x /bin/funlinux
-cat ./.alias >> /$HOME/.bashrc
+cat ./alias >> /$HOME/.bashrc
 mkdir /usr/local/share/funlinux
-cp ./.donkey /usr/local/share/funlinux
-cp ./.badtime.mp3 /usr/local/share/funlinux
+cp ./donkey /usr/local/share/funlinux
+cp ./badtime.mp3 /usr/local/share/funlinux
 cp -r ./waifu /usr/local/share/funlinux
 
 
